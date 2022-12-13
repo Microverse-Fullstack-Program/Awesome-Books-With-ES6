20aa6d4f0872b359b905b394a2c0e2a24f4952da
cherelemma <73167960+cherelemma@users.noreply.github.com>
2022-12-13 16:22:16 +0300

    Refactor regular fucntions to arrow function

--- awesome-books/modules/Date.js
+++ awesome-books/modules/Date.js
@@ -3,8 +3,10 @@ import { DateTime } from './luxon.js';
 // Date & Time Display Function
 const dateContainer = document.getElementById('date');
 const currentDate = DateTime.now();
-export default function displayDateTime() {
+const displayDateTime = () => {
   const dateString = currentDate.toLocaleString(DateTime.DATETIME_MED);
   dateContainer.textContent = dateString;
   setInterval(displayDateTime, 1000);
 }
+
+export default displayDateTime;
--- awesome-books/modules/displayRemove.js
+++ awesome-books/modules/displayRemove.js
@@ -1,7 +1,7 @@
 const booksContainer = document.querySelector('#book-list');
 
 // Function displayBooks.
-export default function displayBooks({ title, author, id }) {
+const displayBooks = ({ title, author, id }) => {
   const newBook = document.createElement('div');
   newBook.id = id;
   newBook.className = 'book-wrapper';
@@ -41,3 +41,5 @@ export default function displayBooks({ title, author, id }) {
     });
   });
 }
+
+export default displayBooks;
